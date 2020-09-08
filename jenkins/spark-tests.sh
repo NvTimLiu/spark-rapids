7f1f37528e899faa386a085fd213136c221330d5
MG <gamem.8818@gmail.com>
2020-09-08 19:34:41 +0800

    Update spark-tests.sh

--- jenkins/spark-tests.sh
+++ jenkins/spark-tests.sh
@@ -71,7 +71,9 @@ BASE_SPARK_SUBMIT_ARGS="--master spark://$HOSTNAME:7077 --executor-memory 32G \
     --conf spark.executor.extraJavaOptions=-Duser.timezone=GMT \
     --conf spark.sql.session.timeZone=UTC \
     --conf spark.rapids.python.memory.gpu.pooling.enabled=false \
-    --conf spark.rapids.python.concurrentPythonWorkers=1 "
+    --conf spark.rapids.python.concurrentPythonWorkers=1 \
+    --conf spark.executorEnv.PYTHONPATH=${RAPIDS_PLUGIN_JAR} \
+    --py-files ${RAPIDS_PLUGIN_JAR} "
 MORTGAGE_SPARK_SUBMIT_ARGS=" --conf spark.plugins=com.nvidia.spark.SQLPlugin \
     --class com.nvidia.spark.rapids.tests.mortgage.Main \
     $RAPIDS_TEST_JAR"
