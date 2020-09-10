9aa4847a7758073e93c4f9b447a037adb8263b9b
MG <gamem.8818@gmail.com>
2020-09-10 17:31:24 +0800

    Update marks.py

--- integration_tests/src/main/python/marks.py
+++ integration_tests/src/main/python/marks.py
@@ -20,4 +20,4 @@ ignore_order = pytest.mark.ignore_order
 incompat = pytest.mark.incompat
 limit = pytest.mark.limit
 qarun = pytest.mark.qarun
-cudfudf = pytest.mark.cudfudf
+cudf_udf = pytest.mark.cudf_udf
