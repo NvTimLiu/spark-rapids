df9e483fade0e853b6c5b841177fcc4902a00f86
MG <gamem.8818@gmail.com>
2020-09-10 18:57:25 +0800

    Update spark-tests.sh

--- jenkins/spark-tests.sh
+++ jenkins/spark-tests.sh
@@ -69,6 +69,7 @@ BASE_SPARK_SUBMIT_ARGS="--master spark://$HOSTNAME:7077 --executor-memory 32G \
     --conf spark.executor.extraClassPath=${CUDF_JAR}:${RAPIDS_PLUGIN_JAR} \
     --conf spark.driver.extraJavaOptions=-Duser.timezone=GMT \
     --conf spark.executor.extraJavaOptions=-Duser.timezone=GMT \
+    --conf spark.rapids.sql.python.gpu.enabled=false \
     --conf spark.sql.session.timeZone=UTC"
 MORTGAGE_SPARK_SUBMIT_ARGS=" --conf spark.plugins=com.nvidia.spark.SQLPlugin \
     --class com.nvidia.spark.rapids.tests.mortgage.Main \
@@ -77,7 +78,6 @@ MORTGAGE_SPARK_SUBMIT_ARGS=" --conf spark.plugins=com.nvidia.spark.SQLPlugin \
 UDF_CUDF_TEST_ARGS="--conf spark.rapids.python.memory.gpu.pooling.enabled=false \
     --conf spark.rapids.memory.gpu.pooling.enabled=true \
     --conf spark.rapids.memory.gpu.allocFraction=0.4 \
-    --conf spark.rapids.sql.python.gpu.enabled=true \
     --conf spark.executorEnv.PYTHONPATH=rapids-4-spark_2.12-0.2.0-SNAPSHOT.jar \
     --py-files ${RAPIDS_PLUGIN_JAR}"
 
